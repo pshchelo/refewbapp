b0979609271621677cc1faa8626bf4445e660688
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2020-03-30 21:29:13 +0300

    Add minimal REST API for Records
    - `POST /records` to create record
    - `GET /records` to list all records
    - `GET /records/{id}` to get record by ID
    
    Record has:
    - autogenerated integer ID
    - autogenerated crated_at time
    - autoinserted host name of the host that made the record
    
    No validation for now (as it accepts no inputs for now).
    In the future will also contain simple text payload data.
    
    No updates, no deletes as this supposed to be quite ephemeral.

--- refwebapp/models.py
+++ refwebapp/models.py
@@ -1,15 +1,32 @@
+import datetime
+import platform
+import uuid
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 
 Base = declarative_base()
 
 
 class Record(Base):
-    __tablename__ = 'employees'
-    id = Column(Integer, primary_key=True)
-    uuid = Column(String(50))
-    data = Column(String(50), nullable=True)
-    host = Column(String(255))
+    __tablename__ = 'records'
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    data = Column(String(255), nullable=True)
+    host = Column(String(255), nullable=False, default=platform.node())
+
+    # def __init__(self, data=None):
+    #     super().__init__(self)
+    #     if data is not None:
+    #         self.data = data
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            created_at=self.created_at.isoformat(timespec="milliseconds"),
+            host=self.host,
+            data=self.data
+        )
 
 
 def init(engine):
--- refwebapp/records.py
+++ refwebapp/records.py
@@ -1,14 +1,36 @@
-from . import models as m
+import json
+import logging
+
+import falcon
+
+from .models import Record
+
+LOG = logging.getLogger(__name__)
 
 
 class RecordResourceCollection:
     def on_get(self, req, resp):
-        pass
+        LOG.debug("Listing Records ...")
+        result = req.context.session.query(Record).all()
+        LOG.info(f"Got {len(list(result))} Records")
+        resp.body = json.dumps(dict(records=list(r.to_dict() for r in result)))
 
-    def on_post(self, req, resp, id):
-        pass
+    def on_post(self, req, resp):
+        LOG.debug("Creating Record ...")
+        new_rec = Record()
+        req.context.session.add(new_rec)
+        LOG.debug("Committing Record ...")
+        req.context.session.commit()
+        LOG.info(f"Created record {new_rec.id} with data {new_rec.data}")
+        resp.body = json.dumps(new_rec.to_dict())
 
 
 class RecordResource:
     def on_get(self, req, resp, id):
-        pass
+        LOG.debug(f"Accessing Record {id} ...")
+        result = req.context.session.query(Record).get(id)
+        if result:
+            resp.body = json.dumps(result.to_dict())
+        else:
+            LOG.info("No Record with id {id} found")
+            resp.status = falcon.HTTP_404
